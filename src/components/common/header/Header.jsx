d0c1a331f85e3429563d3b585ca3462656a19073
Maria Salazar <mariatrinidadsalazarr@gmail.com>
2024-08-16 04:42:26 -0400

    Change: Footer information, Header

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header>
       <div className="mt-8">
         <h1 className="font-pop font-black sm:text-xl lg:text-4xl 2xl:text-6xl text-center text-white animate-slide ">
-          Encuentra restaurantes cercanos
+          descubre las claves de exito de los restaurantes mejores calificados
         </h1>
       </div>
     </header>
