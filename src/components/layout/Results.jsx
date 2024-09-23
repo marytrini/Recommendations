5e8e1c00b14f032432ec76527a9e8e28dcbe2dca
Maria Salazar <mariatrinidadsalazarr@gmail.com>
2024-09-23 00:43:07 -0400

    refactor: restructure renderization of results with a modal

--- src/components/layout/Results.jsx
+++ src/components/layout/Results.jsx
@@ -1,20 +1,44 @@
 import PropTypes from "prop-types";
 
-const Results = ({ results }) => {
+const Results = ({ openModal, closeModal, results }) => {
+  const handleCloseModal = (event) => {
+    if (event.target.id === "modalContainer") {
+      closeModal();
+    }
+  };
+
+  if (openModal !== true) return null;
+
   return (
-    <div className="sm:w-32 lg:w-60 h-auto p-6 rounded-md bg-card-blocks">
-      <h2 className="font-pop text-lg font-bold text-white hover:text-amber-500">
-        Resultados:{" "}
-      </h2>
-      <p className="font-pop text-sm font-semibold text-white hover:text-amber-500">
-        {results}
-      </p>
+    <div
+      className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50" // Fondo transparente oscuro
+      id="modalContainer"
+      onClick={handleCloseModal}
+    >
+      <div className="p-8 rounded-lg shadow-lg w-96 bg-card-bg">
+        {" "}
+        {/* Fondo rojo solo en el modal */}
+        <button
+          onClick={closeModal}
+          className="text-white bg-amber-500 p-2 rounded-full"
+        >
+          &times;
+        </button>
+        <h2 className="font-pop text-lg font-bold text-white hover:text-amber-500">
+          Resultados:{" "}
+        </h2>
+        <p className="font-pop text-sm font-semibold text-white hover:text-amber-500">
+          {results}
+        </p>
+      </div>
     </div>
   );
 };
 
 Results.propTypes = {
-  results: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired, // Asegura que results sea un string o un object y sea requerido
+  openModal: PropTypes.bool.isRequired, // Validación de openModal
+  closeModal: PropTypes.func.isRequired, // Validación de closeModal
+  results: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired, // Validación de results
 };
 
 export default Results;
